c960bafce4e1929494912097ecc24e879425a9fc
Petr Vlcek <139757280+born4web-iot@users.noreply.github.com>
2023-10-24 20:39:54 +0200

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 def on_button_pressed_a():
-    basic.show_string("0")
-    servos.P0.set_angle(0)
-    basic.pause(1000)
-    basic.clear_screen()
+    Servo180_StepDown()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def Servo180_StepUp():
+    global SERVO_180_ANGLE
+    SERVO_180_BTN_STEP = 0
+    SERVO_180_ANGLE += 0 + SERVO_180_BTN_STEP
+    if SERVO_180_ANGLE > 180:
+        SERVO_180_ANGLE = 180
+def Servo180_RunTest():
+    basic.show_string("" + str(SERVO_180_ANGLE))
+    servos.P0.set_angle(SERVO_180_ANGLE)
+def Servo180_StepDown():
+    global SERVO_180_ANGLE
+    SERVO_180_BTN_STEP2 = 0
+    SERVO_180_ANGLE += 0 - SERVO_180_BTN_STEP2
+    if SERVO_180_ANGLE < 0:
+        SERVO_180_ANGLE = 0
+
 def on_button_pressed_b():
-    basic.show_string("1")
-    servos.P0.set_angle(90)
-    basic.pause(1000)
-    basic.clear_screen()
+    Servo180_StepUp()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def Servo180_SetUp():
+    global SERVO_180_ANGLE
+    SERVO_180_ANGLE += 10
+    SERVO_180_ANGLE = 0
+SERVO_180_ANGLE = 0
 basic.show_icon(IconNames.HAPPY)
 basic.pause(1000)
 basic.clear_screen()
+Servo180_SetUp()
 
 def on_forever():
-    pass
+    Servo180_RunTest()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,18 +1,36 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("0")
-    servos.P0.setAngle(0)
-    basic.pause(1000)
-    basic.clearScreen()
+    Servo180_StepDown()
 })
+function Servo180_StepUp () {
+    SERVO_180_ANGLE += 0 + SERVO_180_BTN_STEP
+    if (SERVO_180_ANGLE > 180) {
+        SERVO_180_ANGLE = 180
+    }
+}
+function Servo180_RunTest () {
+    basic.clearScreen()
+    basic.showString("" + SERVO_180_ANGLE)
+    servos.P2.setAngle(SERVO_180_ANGLE)
+}
+function Servo180_StepDown () {
+    SERVO_180_ANGLE += 0 - SERVO_180_BTN_STEP
+    if (SERVO_180_ANGLE < 0) {
+        SERVO_180_ANGLE = 0
+    }
+}
 input.onButtonPressed(Button.B, function () {
-    basic.showString("1")
-    servos.P0.setAngle(90)
-    basic.pause(1000)
-    basic.clearScreen()
+    Servo180_StepUp()
 })
+function Servo180_SetUp () {
+    SERVO_180_ANGLE = 0
+    SERVO_180_BTN_STEP = 10
+}
+let SERVO_180_BTN_STEP = 0
+let SERVO_180_ANGLE = 0
 basic.showIcon(IconNames.Happy)
 basic.pause(1000)
 basic.clearScreen()
+Servo180_SetUp()
 basic.forever(function () {
-	
+    Servo180_RunTest()
 })
