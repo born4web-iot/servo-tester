e1f1a758b0b1a72d54c816ab33f5a49e4e4eb891
Petr Vlcek <139757280+born4web-iot@users.noreply.github.com>
2023-10-24 21:01:32 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
+function Servo180_Park_0 () {
+    servos.P2.setAngle(0)
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . . . .
+        `)
+}
 input.onButtonPressed(Button.A, function () {
-    Servo180_StepDown()
+    Servo180_Park_0()
 })
 function Servo180_StepUp () {
     SERVO_180_ANGLE += 0 + SERVO_180_BTN_STEP
@@ -19,18 +29,41 @@ function Servo180_StepDown () {
     }
 }
 input.onButtonPressed(Button.B, function () {
-    Servo180_StepUp()
+    Servo180_Park_180()
+})
+function Servo180_Park_180 () {
+    servos.P2.setAngle(180)
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . . . .
+        `)
+}
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    Servo180_Park_90()
 })
 function Servo180_SetUp () {
     SERVO_180_ANGLE = 0
     SERVO_180_BTN_STEP = 10
 }
+function Servo180_Park_90 () {
+    servos.P2.setAngle(90)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+}
 let SERVO_180_BTN_STEP = 0
 let SERVO_180_ANGLE = 0
 basic.showIcon(IconNames.Happy)
+Servo180_SetUp()
 basic.pause(1000)
 basic.clearScreen()
-Servo180_SetUp()
 basic.forever(function () {
-    Servo180_RunTest()
+	
 })
